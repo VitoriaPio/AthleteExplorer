9a94f46a41f9b409aadaa3b469be4b044a7fe2d1
Vitória Pio <81269768+VitoriaPio@users.noreply.github.com>
2023-12-11 20:31:12 -0300

    thingspeak-config.js

--- src/utils/thingspeak-config.js
+++ src/utils/thingspeak-config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // URL da API
-export const BASE_URL = 'https://api.thingspeak.com/channels/2376505/fields/1.json?results=2';
+export const BASE_URL = 'https://api.thingspeak.com/channels/2376505/feeds.json?results=2';
 
 export const thingspeakAPIConfig = axios.create({
   baseURL: BASE_URL,
