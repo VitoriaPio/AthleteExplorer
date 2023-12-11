78267c452730c45af8d81b2b0ac48486a2759880
Vitória Pio <81269768+VitoriaPio@users.noreply.github.com>
2023-12-11 20:28:03 -0300

    index.jsx

--- src/routes/Iot/index.jsx
+++ src/routes/Iot/index.jsx
@@ -21,9 +21,6 @@ function IoT() {
         <h1>Temperatura:</h1>
 
         <h3><span>{Math.round(data.data.feeds[0].field1)}</span></h3>
-        
-        <h1>Umidade:</h1>
-        <h3><span>{Math.round(data.data.feeds[1].field1)}</span></h3>
       </div>
     </div>
   );
